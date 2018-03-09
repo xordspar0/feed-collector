f02cd0de9798b3111eedf470c896b519af3b7e1b
Jordan Christiansen <Jordan.Christiansen@target.com>
2018-03-09 10:18:23 -0600

    Use urfave/cli

--- main.go
+++ main.go
@@ -4,14 +4,29 @@ import (
 	"neolog.xyz/feed-collector/server"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
 
 	"fmt"
+	"os"
 )
 
 var servername = "feed-collector"
 var version = "devel"
 
 func main() {
+	app := cli.NewApp()
+	app.Name = servername
+	app.Usage = "An API that collects your news feeds"
+	app.Action = run
+	app.Version = version
+	app.HideHelp = true
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func run(c *cli.Context) {
 	log.Info(fmt.Sprintf("Starting %s version %s", servername, version))
 	s := server.New()
 	err := s.Start()
